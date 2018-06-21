2df6b77ca8a8e7b2bcd05a17c841a9d68866bd90
Christian Werner <christian.werner@senckenberg.de>
2018-06-21 12:56:47 +0200

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # fastclass
-Little tool to weed through images, delete and classify them into groups for building NN image datasets (based on tkinter)
+A little tool to weed through images, delete and classify them into groups for building NN image datasets (based on tkinter).
+
+## Usage
+Simply install the requirements (if you are using Python2 you need to install Tkinter, not tkinter). Then call the scrpt from the commandline. If you omit any input parameters it will show you the help page.
+
+```
+Usage: fastclass.py [OPTIONS] INFOLDER [OUTFILE]
+
+  FastClass image classifier
+
+Options:
+  -c, --copy         Copy files into class folder after completion  [default:
+                     False]
+  -d, --delete TEXT  Exclude deleted files from output csv file  [default:
+                     True]
+  -e, --ext TEXT     Image file extention to look for  [default: jpg]
+  -w, --wipe TEXT    Wipe exiting output folders if copy is requested
+                     [default: False]
+  -h, --help         Show this message and exit.
+
+  ::: FastClass ::: ...a fast way to cleanup/ sort your images when building
+  a dataset for conv nets.
+
+  Note: In the application use the following keys: <1>, <2> for class
+  assignments <d> to mark a deletion <x> to terminate the app/ write output
+
+  Use the buttons to navigate back and forth without changing the
+  classification. The current classification of an image is given in the
+  title bar (X indicated a mark for deletion). The counter in the titlebar
+  gives number of classified images vs the total number in the input folder.
+```
+
+
---
+++ fastclass.py
@@ -0,0 +1,260 @@
+import click
+import glob
+import os
+from PIL import ImageTk, Image
+import tkinter as tk
+import shutil
+
+EPILOG = """::: FastClass :::\r
+...a fast way to cleanup/ sort your images when building a\r
+dataset for conv nets.\r
+\r
+Note:\r
+In the application use the following keys:\r
+<1>, <2> for class assignments\r
+<d> to mark a deletion\r
+<x> to terminate the app/ write output\r
+\r
+Use the buttons to navigate back and forth without changing\r
+the classification. The current classification of an image\r
+is given in the title bar (X indicated a mark for deletion).\r
+The counter in the titlebar gives number of classified images\r
+vs the total number in the input folder."""
+
+
+class AppTk(tk.Frame):
+    def __init__(self, *args, **kwargs):
+
+        INFOLDER = kwargs['infolder']
+        OUTFILE  = kwargs['outfile']
+        COPY     = kwargs['copy']
+        EXT      = kwargs['file_ext']
+        DEL      = kwargs['delete']
+        WIPE     = kwargs['wipe']
+ 
+        for e in ['infolder', 'outfile', 'copy', 'file_ext', 'delete', 'wipe']:
+            kwargs.pop(e)
+
+        tk.Frame.__init__(self,*args,**kwargs)
+
+        # bind keys
+        args[0].bind('<Key>', self.callback)
+        self.root = args[0]
+
+        # store classification
+        self.c1 = set()
+        self.c2 = set()
+        self.delete  = set()
+
+        # config settings
+        self.filelist = sorted(glob.glob(INFOLDER + '/*.' + EXT))
+        self.outfile = OUTFILE
+        self.infolder = INFOLDER
+        self.exclude_del = DEL 
+        self.copy = COPY        
+        self.wipe = WIPE
+
+        self.index=-1
+        
+        # basic setup
+        self.setup()
+
+        # raise window to top
+        self.root.lift()
+        self.root.attributes("-topmost", True)
+    
+        # show first image
+        self.display_next()
+        
+    def callback(self, event=None):
+        #_key = '{k!r}'.format(k = event.char)
+        if event.char == "1":
+            print("Class 1")
+            #self.ButtonA.config(bg='yellow')
+            #self.root.after(100, lambda: self.ButtonA.config(bg='lightgrey'))
+            self.c1.add(self.filelist[self.index])
+            self.display_next()
+        elif event.char == "2":
+            print("Class 2")
+            #self.ButtonB.config(bg='yellow')
+            #self.root.after(100, lambda: self.ButtonB.config(bg='lightgrey'))
+            self.c2.add(self.filelist[self.index])
+            self.display_next()
+        elif event.char == "d":
+            print("DELETE!")
+            self.delete.add(self.filelist[self.index])
+            self.display_next()
+
+        elif event.keysym == 'Left': #'<Left>':
+            self.display_prev()
+
+        elif event.keysym == 'Right': #'<Right>':
+            self.display_next()
+
+
+        elif event.char == "x":
+            print("Results:")
+            
+            print("\nCLASS 1:")
+            for i in sorted(list(self.c1)):
+                print(i)
+
+            print("\nCLASS 2:")
+            for i in sorted(list(self.c2)):
+                print(i)
+
+            print("\nDELETE:")
+            for i in sorted(list(self.delete)):
+                print(i)
+
+            # create output folders
+            if self.copy:
+                ds = ['class1_files', 'class2_files']
+
+                if os.path.exists(ds[0]):
+                    if self.wipe:
+                        if os.path.exists(ds[0]):
+                            print("Wiping existing output folder!")
+                            for d in ds:
+                                shutil.rmtree('/%s' % d)
+                    else:
+                        print("Output folders exist and wipe was not requested.")
+                        print("Will not copy folders...")
+                        self.copy = False
+
+                for d in ds:
+                    if not os.path.exists(d):
+                        os.makedirs(d)
+
+
+            if not self.outfile:
+                self.outfile = open(os.path.basename(self.infolder) + '_classified.csv', 'w')
+            for f in self.filelist:
+                if f in self.c1:
+                    x = '1'
+                    if self.copy:
+                        shutil.copy(f, ds[0])
+                elif f in self.c2:
+                    x = '2'
+                    if self.copy:
+                        shutil.copy(f, ds[1])                    
+                elif f in self.delete:
+                    if self.exclude_del:
+                        continue
+                    x = '-1'
+                else:
+                    # no decision ignore for output
+                    x = '0'                    
+                    continue
+                out = f + ';' + x + '\n'
+                self.outfile.write(out)
+
+            self.outfile.close()
+
+            self.root.destroy()  
+        else:
+            pass
+
+
+    def printSomething(self):
+        labeled = " - "
+        if self.filelist[self.index] in self.c1:
+            labeled += "[ 1 ] " 
+        if self.filelist[self.index] in self.c2:
+            labeled += "[ 2 ] "
+        if self.filelist[self.index] in self.delete:
+            labeled += "[ X ] "
+
+        total = len(self.filelist) 
+        classified = len(self.c1) + len(self.c2) + len(self.delete)
+
+        labeled += " (%d/%d)" % (classified, total)
+
+        self.root.title(os.path.basename( self.filelist[self.index]) + labeled )
+
+    def setup(self):
+        self.Label=tk.Label(self)
+        self.Label.grid(row=0, column=0, columnspan=6, rowspan=6) #, sticky=tk.N+tk.S)
+
+        self.Button=tk.Button(self, text="Prev", command=self.display_prev)
+        self.Button.grid(row=5, column=7, sticky=tk.S)
+        self.Button=tk.Button(self, text="Next", command=self.display_next)
+        self.Button.grid(row=5, column=8, sticky=tk.S)
+        
+
+    def display_next(self):
+        self.index+=1
+        try:
+            f=self.filelist[self.index]
+        except IndexError:
+            self.index=-1  #go back to the beginning of the list.
+            self.display_next()
+            return
+
+        photoimage = ImageTk.PhotoImage(Image.open(f))
+        self.Label.config(image=photoimage)
+        self.Label.image=photoimage
+        self.printSomething()
+
+
+    def display_prev(self):
+        self.index-=1
+        try:
+            f=self.filelist[self.index]
+        except IndexError:
+            self.index=-1  #go back to the beginning of the list.
+            self.display_next()
+            return
+
+        photoimage = ImageTk.PhotoImage(Image.open(f))
+        self.Label.config(image=photoimage)
+        self.Label.image=photoimage
+        self.printSomething()
+
+
+def main(INFOLDER, OUTFILE, COPY, EXT, DEL, WIPE):
+    root = tk.Tk()
+    root.title('FastClass')
+    
+    app = AppTk(root, infolder=INFOLDER, outfile=OUTFILE, copy=COPY, file_ext=EXT, delete=DEL, wipe=WIPE) 
+    app.grid(row=0,column=0)
+
+    # start of nasty hack to give app focus on a mac -----------------------------
+    from os import system
+    from platform import system as platform
+    if platform() == 'Darwin':  # How Mac OS X is identified by Python
+        system('''/usr/bin/osascript -e 'tell app "Finder" to set frontmost of process "Python" to true' ''')
+    # end of nasty hack to give app focus on a mac -----------------------------
+
+    # start event loop
+    root.mainloop()
+
+# CLi - command line arguments
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+click.Context.get_usage = click.Context.get_help
+
+@click.command(context_settings=CONTEXT_SETTINGS, epilog=EPILOG)
+@click.option('-c', '--copy',  is_flag=True, 
+                    default=False, show_default=True,
+                    help='Copy files into class folder after completion')
+
+@click.option('-d', '--delete',  default=True, show_default=True,
+                    help='Exclude deleted files from output csv file')
+
+@click.option('-e', '--ext',  default='jpg', show_default=True,
+                    help='Image file extention to look for')
+
+@click.option('-w', '--wipe',  default=False, show_default=True,
+                    help='Wipe exiting output folders if copy is requested')
+
+
+@click.argument('infolder', type=click.Path(exists=True), required=True)
+@click.argument('outfile', type=click.File('w'), required=False)
+
+def cli(infolder, outfile, copy, ext, delete, wipe):
+    """FastClass image classifier"""
+
+    main(infolder, outfile, copy, ext, delete, wipe)
+
+if __name__ == "__main__":
+    cli()
