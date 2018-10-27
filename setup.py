3958ba2464212817c23238a7cadb017fa9e2c87c
Christian Werner <chr.werner@me.com>
2018-10-27 16:19:18 +0200

    Move to version 0.1.2

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ requirements = to_list("""
 
 setup(
     name = 'fastclass',
-    version = '0.1.1',
+    version = '0.1.2',
     packages = find_packages(),
     install_requires = requirements,
     python_requires  = '>=3.6',
